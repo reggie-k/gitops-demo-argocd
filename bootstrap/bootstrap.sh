36f259651a0c74e09e6c221be7b8d54f7441b652
voloshins <reginakagan@gmail.com>
2019-12-03 22:01:09 +0200

    wait for project deletion nicely

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -16,3 +16,7 @@ sleep 15
 
 # Create the argo user-app 
 oc apply -f ../argo-resources/argo-user-apps.yaml
+
+# Print the initial password (it is the name of the argocd-server pod)
+ARGOCD_SERVER_PASSWORD=$(oc -n argocd get pod -l "app.kubernetes.io/name=argocd-server" -o jsonpath='{.items[*].metadata.name}')
+echo $ARGOCD_SERVER_PASSWORD
--- teardown/teardown.sh
+++ teardown/teardown.sh
@@ -7,6 +7,9 @@ oc delete Application.argoproj.io user-apps -n argocd
 # Delete the Argo project
 oc delete Appprojects default -n argocd
 
+# Will use this for notification that the project got actually deleted
+argocd_proj_exists=`oc get projects | grep argocd | wc -l`
+
 # Then delete the project itself
 oc delete project argocd
 
@@ -18,5 +21,6 @@ oc delete clusterrolebindings argocd-server argocd-application-controller
 oc delete crd applications.argoproj.io
 oc delete crd appprojects.argoproj.io
 
-# Sleep cause it takes time
-sleep 10
+# Wait till the project is actually deleted
+echo 'Waiting for the project to be deleted.....'
+while [ $argocd_proj_exists -eq 1 ]; do argocd_proj_exists=`oc get projects | grep argocd | wc -l`;done
