2a6cf4625b813a91d44df0afc8d7f9f70cf8b370
voloshins <reginakagan@gmail.com>
2019-12-08 15:31:15 +0200

    user resource folder rename

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-set -e
+# set -e
 
 # Create a project for Argo to live in
-oc new-project gitops-infra
+oc new-project argocd
 
 # Install Argo
-oc apply -n gitops-infra -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml 
+oc apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml 
 
 # Create the Route
 oc apply -f ../argo-resources/route.yaml
@@ -18,5 +18,5 @@ sleep 15
 oc apply -f ../argo-resources/argo-user-apps.yaml
 
 # Print the initial password (it is the name of the argocd-server pod)
-ARGOCD_SERVER_PASSWORD=$(oc -n gitops-infra get pod -l "app.kubernetes.io/name=argocd-server" -o jsonpath='{.items[*].metadata.name}')
+ARGOCD_SERVER_PASSWORD=$(oc -n argocd get pod -l "app.kubernetes.io/name=argocd-server" -o jsonpath='{.items[*].metadata.name}')
 echo $ARGOCD_SERVER_PASSWORD
