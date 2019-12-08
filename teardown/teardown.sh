fc77d42603e94c63dbc896067c41941351267b9a
voloshins <reginakagan@gmail.com>
2019-12-08 22:18:31 +0200

    user folder path in umbrella app

--- teardown/teardown.sh
+++ teardown/teardown.sh
@@ -2,16 +2,16 @@
 #set -e
 
 # Delete the Argo apps first (by deleting the umbrella app)
-oc delete Application.argoproj.io user-apps -n gitops-infra
+oc delete Application.argoproj.io user-apps -n argocd
 
 # Delete the Argo project
-oc delete Appprojects default -n gitops-infra
+oc delete Appprojects default -n argocd
 
 # Will use this for notification that the project got actually deleted
-argocd_proj_exists=`oc get projects | grep gitops-infra | wc -l`
+argocd_proj_exists=`oc get projects | grep argocd | wc -l`
 
 # Then delete the project itself
-oc delete project gitops-infra
+oc delete project argocd
 
 # Delete the cluster scope resources
 oc delete clusterrole argocd-server argocd-application-controller
@@ -23,4 +23,4 @@ oc delete crd appprojects.argoproj.io
 
 # Wait till the project is actually deleted
 echo 'Waiting for the project to be deleted.....'
-while [ $argocd_proj_exists -eq 1 ]; do argocd_proj_exists=`oc get projects | grep gitops-infra | wc -l`;done
+while [ $argocd_proj_exists -eq 1 ]; do argocd_proj_exists=`oc get projects | grep argocd | wc -l`;done
